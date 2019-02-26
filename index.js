213d2f3b446a15d3fa23f506a4e3a0d9b5ddb149
王琳飞 <linfei6@staff.sina.com.cn>
2019-02-26 15:30:15 +0800

    tj

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
-app.listen(port,function(){
-  console.log('Webhook service running at http://localhost:'+port);
+app.listen(port, function() {
+    console.log('Webhook service running at http://localhost:' + port);
 });
-app.all('/*', function(req,res,next){
-  if(req.path=='/'){
-    res.redirect(301, 'http://wzytop.top');
-  }
-  next();
+app.all('/*', function(req, res, next) {
+    console.log(req.path)
+    if (req.path == '/' || req.path == '//') {
+        res.redirect(301, 'http://wzytop.cn');
+    }
+    next();
 });
 var hook = new Webhook({
-  port:port,
-  app:app
+    port: port,
+    app: app
 });
